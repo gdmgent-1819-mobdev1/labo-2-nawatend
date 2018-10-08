4e819b6fad297ef40a8e213528db8359985a005f
nawatend <32384185+nawatend@users.noreply.github.com>
2018-10-08 10:36:51 +0200

    able to change like en dislike

--- Tinder v1/js/index.js
+++ Tinder v1/js/index.js
@@ -1,6 +1,9 @@
 let arrOfTenProfile = [];
 let btnLike = document.getElementsByClassName("chooseContainer__like")[0];
 let btnDislike = document.getElementsByClassName("chooseContainer__dislike")[0];
+let btnPreviouse = document.getElementsByClassName(
+  "chooseContainer__previouse"
+)[0];
 let name = document.getElementsByClassName("profileContainer__info--name")[0];
 let age = document.getElementsByClassName("profileContainer__info--age")[0];
 let locationPerson = document.getElementsByClassName(
@@ -10,14 +13,50 @@ let image = document.getElementsByClassName(
   "profileContainer__images--image"
 )[0];
 let profileContainer = document.getElementsByClassName("profileContainer")[0];
-let destroy = document.getElementsByClassName("destroy")[0];
+let clear = document.getElementsByClassName("clear")[0];
 
+//profiles counter variable en initiated
 let profileCounter = 1;
 let nextGroupOfProfileCounter = 1;
 let profileAddedCounter = 1;
+
+//Choosing Gender In menu -------------------------- !!! MENU FEATURE WERK NIET !!!
+let female = document.getElementsByClassName("menu__burger--female")[0];
+let male = document.getElementsByClassName("menu__burger--male")[0];
+let both = document.getElementsByClassName("menu__burger--X ")[0];
+let url = "https://randomuser.me/api/?results=10";
+
+female.addEventListener("click", function() {
+  url = "https://randomuser.me/api/?gender=female";
+  UpateProfilesByGenderChosen();
+
+  console.log(url);
+});
+male.addEventListener("click", function() {
+  url = "https://randomuser.me/api/?gender=male";
+  UpateProfilesByGenderChosen();
+
+  console.log(url);
+});
+
+both.addEventListener("click", function() {
+  url = "https://randomuser.me/api/";
+  UpateProfilesByGenderChosen();
+
+  console.log(url);
+});
+
+//Update Profiles to Chosen Gender
+function UpateProfilesByGenderChosen() {
+  localStorage.clear();
+  setTenProfilesInLocalStorage();
+}
+
+//LOAD
+
+// Dit is geen efficient want x10 fetch
 function setTenProfilesInLocalStorage() {
   for (let i = 0; i < 10; i++) {
-    let url = "https://randomuser.me/api/?gender=female";
     fetch(url, {
       method: "GET"
     })
@@ -25,12 +64,8 @@ function setTenProfilesInLocalStorage() {
         return response.json();
       })
       .then(function(data) {
-        // binnen deze functie kan je de json data manipuleren
+        //Binnen deze functie kan je de json data manipuleren
         //we need name, age, location, foto,
-        //location: data.results[0].location.city
-        //foto:     data.results[0].picture.large
-        //name:     data.results[0].name.first
-        // age:     data.results[0].dob.age
 
         let profile = [
           data.results[0].name.first,
@@ -45,6 +80,7 @@ function setTenProfilesInLocalStorage() {
           "profile" + profileAddedCounter.toString(),
           JSON.stringify(profile)
         );
+        console.log(JSON.stringify(profile));
         //console.log("Profile Add: " + profileAddedCounter);
         profileAddedCounter++;
       });
@@ -56,42 +92,93 @@ setTenProfilesInLocalStorage();
 // for (let i = 0; i < 10; i++) {
 //   console.log(JSON.parse(localStorage.getItem("profile" + i)));
 // }
-
+//Liking en disliking btns
 btnLike.addEventListener("click", function() {
+  addLikeOfDislikeToProfile("liked");
   goToNextProfile();
 });
-
+btnPreviouse.addEventListener("click", function() {
+  goToPreviouseProfile();
+});
 btnDislike.addEventListener("click", function() {
+  addLikeOfDislikeToProfile("dis");
   goToNextProfile();
 });
 
-profileContainer.addEventListener("load", function() {
-  console.log(
-    JSON.parse(localStorage.getItem("profile" + profileCounter.toString()))
-  );
-  console.log(profileCounter);
-});
+// profileContainer.addEventListener("load", function() {
+//   console.log(
+//     JSON.parse(localStorage.getItem("profile" + profileCounter.toString()))
+//   );
+//   console.log(profileCounter);
+// });
 
-destroy.addEventListener("click", function() {
-  console.log("destroyeed");
+clear.addEventListener("click", function() {
+  console.log("Cleared Storage");
   localStorage.clear();
 });
 
+function addLikeOfDislikeToProfile(likeStatus) {
+  let profileToUpdate = localStorage.getItem("profile" + profileCounter);
+  console.log(profileToUpdate);
+  let likeOrdislike;
+  if (
+    profileToUpdate.search("liked") !== -1 ||
+    profileToUpdate.search("dis") !== -1
+  ) {
+    if (profileToUpdate.search("liked") !== -1) {
+      profileToUpdate = profileToUpdate.replace("liked", likeStatus);
+      console.log("Changed to Dis");
+      localStorage.setItem("profile" + profileCounter, profileToUpdate);
+    } else {
+      profileToUpdate = profileToUpdate.replace("dis", likeStatus);
+      localStorage.setItem("profile" + profileCounter, profileToUpdate);
+    }
+  } else {
+    if (likeStatus === "liked") {
+      likeOrdislike = '","liked"]';
+    } else {
+      likeOrdislike = '","dis"]';
+    }
+
+    let index = profileToUpdate.indexOf("]") - 1;
+
+    profileToUpdate = profileToUpdate.slice(0, index) + likeOrdislike;
+    localStorage.setItem("profile" + profileCounter, profileToUpdate);
+    console.log("not likeds");
+  }
+
+  //console.log(profileToUpdate);
+
+  //Change like or dislike -- search 4th ','
+  let komma4thIndex = profileToUpdate.lastIndexOf(",");
+  //console.log("komme:  " + komma4thIndex);
+  // console.log(profileToUpdate.slice(komma4thIndex - 1, profileToUpdate.length));
+  // console.log(index);
+  // console.log(localStorage.getItem("profile" + profileCounter));
+}
 function goToNextProfile() {
   if (profileCounter < localStorage.length) {
-    showProfile();
-
     if (nextGroupOfProfileCounter === 9) {
       setTenProfilesInLocalStorage();
 
       nextGroupOfProfileCounter = 1;
     } else {
-      addLikeOfDislikeToProfile();
       nextGroupOfProfileCounter++;
     }
 
     profileCounter++;
+    showProfile();
   }
+}
+
+function goToPreviouseProfile() {
+  console.log(profileCounter);
+  if (profileCounter !== 1) {
+    profileCounter--;
+  }
+
+  console.log(profileCounter);
+  showProfile();
 }
 
 function showProfile() {
@@ -109,32 +196,30 @@ function showProfile() {
     "src",
     JSON.parse(localStorage.getItem("profile" + profileCounter))[3]
   );
-  // console.log(JSON.parse(localStorage.getItem("profile" + profileCounter)));
+  console.log(profileCounter);
+  console.log(JSON.parse(localStorage.getItem("profile" + profileCounter)));
   // console.log("ProfileNr: " + profileCounter);
 }
+showProfile();
 
 function removeProfile() {
   localStorage.removeItem("profile" + profileCounter);
 }
 //start with a profile index 0
-showProfile();
+//showProfile();
 
-function addLikeOfDislikeToProfile() {
-  let profileToUpdate = localStorage.getItem("profile" + profileCounter);
+//Everything with menu
+let btnMenu = document.getElementsByClassName("headerContainer__burgermenu")[0];
+let menu = document.getElementsByClassName("menu__burger")[0];
 
-  let like = '","liked"]';
+let menuItems = document.getElementsByClassName("menu__burger--items")[0];
+btnMenu.addEventListener("click", function() {
+  menu.classList.toggle("menu__expand");
+  menuItems.classList.toggle("menu__show");
+  btnMenu.style.color = "white";
+});
 
-  let index = profileToUpdate.indexOf("]") - 1;
-
-  let newString = profileToUpdate.slice(0, index) + like;
-
-  localStorage.setItem("profile" + profileCounter, newString);
-  console.log(index);
-
-  console.log(localStorage.getItem("profile" + profileCounter));
-}
-
-//some test
+//some tests
 let listLocalStorage = document.getElementsByClassName("list__localstorage")[0];
 let btnShowList = document.getElementsByClassName("btn__showList")[0];
 
